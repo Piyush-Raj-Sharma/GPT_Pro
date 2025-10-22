08fc0453f581f054caef4730b9bce933bda8e225
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-23 01:15:20 +0530

    Added DB connection & setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config;
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const connectDB = require('../db/db');
 
 const app = express();
+
+connectDB();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
