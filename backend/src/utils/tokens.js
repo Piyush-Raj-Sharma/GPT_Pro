d6759c311dc512d37085fea2288368f33fa0f4cd
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-24 01:05:40 +0530

    Added util function to create auth tokens

---
+++ backend/src/utils/tokens.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+function generateTokens(user){
+    const accessToken = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+        expiresIn: "15m"
+    });
+
+    const refreshToken = jwt.sign({id: user._id}, process.env.JWT_REFRESH_SECRET, {
+        expiresIn:  "7d"
+    })
+
+    return {accessToken, refreshToken};
+}
+
+module.exports = generateTokens;
