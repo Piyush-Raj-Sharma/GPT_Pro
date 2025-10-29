9ab80f365baeb5a9dcefe6fb2571f8b085134271
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-30 00:47:00 +0530

    feat(socket): Added a middleware for Socket.IO connections that verifies JWT access tokens from cookies. Ensures only authenticated users can establish socket connections.

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,4 +1,7 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
 function initializeSocketServer(httpServer) {
   const io = new Server(httpServer, {
@@ -6,6 +9,23 @@ function initializeSocketServer(httpServer) {
       origin: "*",
       methods: ["GET", "POST"],
     },
+  });
+
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.accessToken)
+      next(new Error("Authentication error: No token provieded"));
+
+    try {
+      const decoded = jwt.verify(cookies.accessToken, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user = user;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error: Invalid token"));
+    }
+    // console.log("Socket connection cookies", cookies);
   });
 
   io.on("connection", (socket) => {
